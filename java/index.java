c9cae22f201308b2bcbabccc4c88a33d4c34f4a7
azmarifdev <azmarif.dev@gmail.com>
2024-05-25 23:14:36 +0600

    first co

--- java/index.java
+++ java/index.java
@@ -1,1 +1,25 @@
-hello dfd
+
+
+conflicted = '${count}⚠ '
+ahead = "↪${count}"
+behind = "${count}"
+diverged = "↕↗${ahead_count}↘${behind_count}"
+untracked = '${count}⁉'
+stashed = '${count}👁️‍🗨️'
+modified = '${count}'
+staged = '${count} '
+renamed = '${count} '
+deleted = '${count} '
+up_to_date = '✓'
+
+conflicted = '${count}⚠'
+ahead = "⇡${count}"
+behind = "⇣${count}"
+diverged = "⇕⇡${ahead_count}⇣${behind_count}"
+untracked = '${count}?'
+stashed = '${count}'
+modified = '${count} '
+staged = '${count} '
+renamed = '${count} '
+deleted = '${count}🔴'
+# up_to_date = '✅'
